0431d2234628376799dc70001cc0ead9915aea8d
Dmitry Lachugin <D.Lachugin@transtelematica.ru>
2025-06-27 12:54:16 +0400

    доработки под тест нагрузки

--- k6_tests/user_search.js
+++ k6_tests/user_search.js
@@ -3,10 +3,10 @@ import { check, sleep } from 'k6';
 import { Trend, Rate, Counter } from 'k6/metrics';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
-const metric_search_latency      = new Trend('search_latency_ms', true); // true - enable averaging
-const metric_search_success_rate = new Rate('search_success_rate');
-const metric_search_errors       = new Counter('search_errors_total');
-const metric_search_duration     = new Trend('search_duration_ms');
+const metric_search_latency  = new Trend('search_latency', true); // true - enable averaging
+const metric_search_success  = new Rate('search_success');
+const metric_search_errors   = new Counter('search_errors');
+const metric_search_duration = new Trend('search_duration');
 
 export let options = {
   stages: [
@@ -18,20 +18,9 @@ export let options = {
   ],
   ext: {
     'prometheus-rw': {
-      url: __ENV.K6_PROMETHEUS_RW_SERVER_URL,
-      metrics: [
-        'search_latency_ms_avg',
-        'search_latency_ms_max',
-        'search_success_rate',
-        'search_errors_total',
-        'search_duration_ms'
-      ],
+      url: __ENV.K6_PROMETHEUS_RW_SERVER_URL
     },
-  },
-  // thresholds: {
-  //   'search_latency_ms': ['p(95)<500'],  // 95% запросов быстрее 500ms
-  //   'search_success_rate': ['rate>0.95'],  // >95% успешных запросов
-  // }
+  }
 };
 
 // тестовые данные
@@ -49,7 +38,7 @@ export default function ()
             // в переменных окружения может быть два варианта
             // - TEST_TYPE=no-index
             // - TEST_TYPE=with-index
-            test_type: __ENV.TEST_TYPE || 'no-index'
+            test_type: __ENV.TEST_TYPE || 'unknown'
         }
     };
     const host = __ENV.TEST_HOST || 'app:6000'
@@ -59,14 +48,14 @@ export default function ()
     const duration = Date.now() - start
 
     metric_search_latency.add(duration, { type: params.tags.test_type });
-    metric_search_duration.add(res.timings.duration);
-    metric_search_success_rate.add(res.status === 200);
-    if (res.status !== 200) metric_search_errors.add(1);
+    metric_search_duration.add(res.timings.duration, { type: params.tags.test_type });
+    metric_search_success.add(res.status === 200, { type: params.tags.test_type });
+    if (res.status !== 200) metric_search_errors.add(1, { type: params.tags.test_type });
 
-    // check(res, {
-    //     'status 200': (r) => r.status === 200,
-    //     'response time <500ms': (r) => r.timings.duration < 500
-    // });
+    check(res, {
+        'status 200': (r) => r.status === 200,
+        'response time <500ms': (r) => r.timings.duration < 500
+    });
 
     sleep(0.1);
 }
