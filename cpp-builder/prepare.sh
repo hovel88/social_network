310c8d94d440ab724b3bd9ca70f7f5b5d2529703
Dmitry Lachugin <lachugindm@gmail.com>
2025-08-14 16:17:44 +0400

    cpp-builder

--- cpp-builder/prepare.sh
+++ cpp-builder/prepare.sh
@@ -5,7 +5,7 @@ TARGET_IMAGE_NAME=alpine-cpp-builder
 TARGET_IMAGE_VERSION=1
 
 docker build \
-    -f Dockerfile.${TARGET_IMAGE_NAME} \
+    -f Dockerfile \
     -t ${TARGET_IMAGE_NAME}:${TARGET_IMAGE_VERSION} .
 
 # -t ${TARGET_IMAGE_NAME}:latest
