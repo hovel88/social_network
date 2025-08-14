a7e07fd55d1bc37dc889c34fb13438a7eacffa50
Dmitry Lachugin <lachugindm@gmail.com>
2025-08-14 15:58:31 +0400

    специальный образ сборщика (alpine-cpp-builder)

---
+++ prepare_cpp-builder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+
+TARGET_IMAGE_NAME=alpine-cpp-builder
+TARGET_IMAGE_VERSION=1
+
+docker build \
+    -f Dockerfile.${TARGET_IMAGE_NAME} \
+    -t ${TARGET_IMAGE_NAME}:${TARGET_IMAGE_VERSION} .
+
+# -t ${TARGET_IMAGE_NAME}:latest
